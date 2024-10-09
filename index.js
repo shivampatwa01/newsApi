dae2b59954cf9f2403122d03ecc9ad9985e64f7b
shivampatwa01 <110782429+shivampatwa01@users.noreply.github.com>
2024-10-09 16:21:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,27 +17,28 @@ app.use("/api/user", userRoutes);
 app.use("/api/news", newsRoutes);
 
 app.use("/", (req, res, next) => {
-      res.send("welcome to news API
-               Endpoints:
+      res.send("welcome to news API");
+      res.send("Endpoints: News Data - Access news articles using the endpoint /api/news. | User Data: Manage user-related information with the endpoint /api/user.");
+});
+// Endpoints:
+// News Data: Access news articles using the endpoint /api/news.
+// User Data: Manage user-related information with the endpoint /api/user.
+// Authentication:
 
-News Data: Access news articles using the endpoint /api/news.
-User Data: Manage user-related information with the endpoint /api/user.
-Authentication:
+// The API implements JWT (JSON Web Token) authentication, ensuring secure login and signup processes for users.
+// Data Models:
 
-The API implements JWT (JSON Web Token) authentication, ensuring secure login and signup processes for users.
-Data Models:
+// Comprehensive data models are in place for both news articles and user accounts, ensuring data integrity and efficient management.
+// CRUD Operations:
 
-Comprehensive data models are in place for both news articles and user accounts, ensuring data integrity and efficient management.
-CRUD Operations:
+// Authenticated users have the ability to perform CRUD (Create, Read, Update, Delete) operations on news articles, enabling dynamic content management.
+// Security Features:
 
-Authenticated users have the ability to perform CRUD (Create, Read, Update, Delete) operations on news articles, enabling dynamic content management.
-Security Features:
+// User authentication is handled securely, safeguarding sensitive information and maintaining a high level of user privacy.
+// User-Friendly:
 
-User authentication is handled securely, safeguarding sensitive information and maintaining a high level of user privacy.
-User-Friendly:
-
-The API is designed to be intuitive and easy to use, making it accessible for developers looking to integrate news functionalities into their applications.");
-})
+// The API is designed to be intuitive and easy to use, making it accessible for developers looking to integrate news functionalities into their applications.");
+//})
 
 app.listen(PORT, () => {
     console.log(`Server started successfully at ${PORT}`);
